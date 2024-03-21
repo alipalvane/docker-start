7f40aa62294c46b3e01da47af767dbb585d3422f
Ali <alipalvane@gmail.com>
2024-03-21 22:12:32 +0330

    add docker-vite to project

--- docker-react/src/App.tsx
+++ docker-react/src/App.tsx
@@ -16,7 +16,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Dokcer is best!..</h1>
+      <h1>Dokcer is best!</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
